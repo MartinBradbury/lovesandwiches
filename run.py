03e6ba1262b2710b6c174bf31d7df2bfd04b11d7
MartinBradbury <>
2023-10-28 16:44:56 +0000

    add requirements for deployment

--- run.py
+++ run.py
@@ -3,7 +3,7 @@
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
 import gspread
 from google.oauth2.service_account import Credentials
-from pprint import pprint
+# from pprint import pprint
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -43,7 +43,7 @@ def get_sales_data():
         /n gives an extra line under the example data
         """
 
-        data_str = input("Enter your data here:")
+        data_str = input("Enter your data here:\n")
         """print(f"The data provided is {data_str}")"""
 
         sales_data = data_str.split(",")
